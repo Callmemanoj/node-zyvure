cc6dc40765c62e2ca8f33950f3dfa17c96e85b07
Callmemanoj <surisetti.manoj143@gmail.com>
2021-05-28 18:51:02 +0530

    3 files modified

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  //res.render('app');
-  res.send({ message: 'hello world' });
+  res.render('app');
+  //res.send({ message: 'hello world' });
 });
 app.listen(port, () => {
   console.log('server listening to port 3000');
